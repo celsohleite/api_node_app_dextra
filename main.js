eda33cf34110d6c74d7a0b801b525f75a5b0ed86
celso henrique leite <celsohleite@gmail.com>
2018-09-13 23:53:14 -0300

    inclusão de novas fotos no serviços
    inclusão de novas urls com imagem, pois duas das imagens estava fora do ar.

--- main.js
+++ main.js
@@ -4,32 +4,96 @@ let app = express()
 
 // For test proposes, the "database" is described bellow
 const ingredients = {
-	1: {id: 1, name: 'Alface', price: 0.40, image: 'https://goo.gl/9DhCgk'},
-	2: {id: 2, name: 'Bacon', price: 2.00, image: 'https://goo.gl/8qkVH0'},
-	3: {id: 3, name: 'Hamburguer de Carne', price: 3.00, image: 'https://goo.gl/U01SnT'},
-	4: {id: 4, name: 'Ovo', price: 0.80, image: 'https://goo.gl/weL1Rj'},
-	5: {id: 5, name: 'Queijo', price: 1.50, image: 'https://goo.gl/D69Ow2'},
-	6: {id: 6, name: 'Pão com gergelim', price: 1.00, image: 'https://goo.gl/evgjyj'}
+	1: {
+		id: 1,
+		name: 'Alface',
+		price: 0.40,
+		image: 'https://goo.gl/9DhCgk'
+	},
+	2: {
+		id: 2,
+		name: 'Bacon',
+		price: 2.00,
+		image: 'https://goo.gl/8qkVH0'
+	},
+	3: {
+		id: 3,
+		name: 'Hamburguer de Carne',
+		price: 3.00,
+		image: 'https://goo.gl/U01SnT'
+	},
+	4: {
+		id: 4,
+		name: 'Ovo',
+		price: 0.80,
+		image: 'https://goo.gl/weL1Rj'
+	},
+	5: {
+		id: 5,
+		name: 'Queijo',
+		price: 1.50,
+		image: 'https://goo.gl/D69Ow2'
+	},
+	6: {
+		id: 6,
+		name: 'Pão com gergelim',
+		price: 1.00,
+		image: 'https://goo.gl/evgjyj'
+	}
 }
-
+//X-Egg e 
 const sandwiches = {
-	1: {id: 1, name: 'X-Bacon', ingredients: [2, 3, 5, 6], image: 'https://goo.gl/W9WdaF'},
-	2: {id: 2, name: 'X-Burger', ingredients: [3, 5, 6], image: 'https://goo.gl/Cjfxi9'},
-	3: {id: 3, name: 'X-Egg', ingredients: [3, 4, 5, 6], image: 'https://goo.gl/x4rNIq'},
-	4: {id: 4, name: 'X-Egg Bacon', ingredients: [1, 2, 3, 4, 5, 6], image: 'https://goo.gl/2L0lqg'}
+	1: {
+		id: 1,
+		name: 'X-Bacon',
+		ingredients: [2, 3, 5, 6],
+		image: 'https://goo.gl/W9WdaF'
+	},
+	2: {
+		id: 2,
+		name: 'X-Burger',
+		ingredients: [3, 5, 6],
+		image: 'https://goo.gl/Cjfxi9'
+	},
+	3: {
+		id: 3,
+		name: 'X-Egg',
+		ingredients: [3, 4, 5, 6],
+		image: 'https://static.expressodelivery.com.br/imagens/produtos/4827/180/Expresso-Delivery_2a753836ab2b68ac7fac38a638de619a.png'
+	},
+	4: {
+		id: 4,
+		name: 'X-Egg Bacon',
+		ingredients: [1, 2, 3, 4, 5, 6],
+		image: 'https://www.zapvendas.com.br/upload/55/f0aa7dd441651f23cef66e3d1f9af1b1.png'
+	}
 }
 
 const promos = {
-	1: {id: 1, name: 'Light', description: 'Se o lanche tem alface e não tem bacon, ganha 10% de desconto.'},
-	2: {id: 2, name: 'Muita carne', description: 'A cada 3 porções de carne só paga 2. Se o lanche tiver 6 porções, pagará 4. Assim por diante'},
-	3: {id: 3, name: 'Muito queijo', description: 'A cada 3 porções de queijo só paga 2. Se o lanche tiver 6 porções, pagará 4. Assim por diante'}
+	1: {
+		id: 1,
+		name: 'Light',
+		description: 'Se o lanche tem alface e não tem bacon, ganha 10% de desconto.'
+	},
+	2: {
+		id: 2,
+		name: 'Muita carne',
+		description: 'A cada 3 porções de carne só paga 2. Se o lanche tiver 6 porções, pagará 4. Assim por diante'
+	},
+	3: {
+		id: 3,
+		name: 'Muito queijo',
+		description: 'A cada 3 porções de queijo só paga 2. Se o lanche tiver 6 porções, pagará 4. Assim por diante'
+	}
 }
 
 let orders = {}
 
 // Basic configuration
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 app.listen(8080)
 
 // Business logic
@@ -39,7 +103,7 @@ const map_to_array = (map) => {
 	return list
 }
 
-const list_endpoints = (res) =>	res.send('Valid REST routes:\n/api/pedido\n/api/ingrediente\n/api/lanche\n/api/promocao')
+const list_endpoints = (res) => res.send('Valid REST routes:\n/api/pedido\n/api/ingrediente\n/api/lanche\n/api/promocao')
 const list_sandwiches = (res) => res.json(map_to_array(sandwiches))
 const list_ingredients = (res) => res.json(map_to_array(ingredients))
 const list_promos = (res) => res.json(map_to_array(promos))
@@ -71,7 +135,12 @@ const add_order = (id_sandwich, extras, res) => {
 	extras = extras ? JSON.parse(extras) : []
 	let [last_id] = Object.keys(orders).slice(-1)
 	let id = last_id ? parseInt(last_id) + 1 : 1
-	let order = {id, id_sandwich, extras, date: new Date().getTime()}
+	let order = {
+		id,
+		id_sandwich,
+		extras,
+		date: new Date().getTime()
+	}
 	orders[id] = order
 	res.json(order)
 }
